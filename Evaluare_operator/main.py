879c91c44e79aecba99d5cc6c24f0a8c37d64b3f
LucianTudor25 <luci.tudor@yahoo.com>
2026-07-04 01:23:13 +0300

    Update main.py corectat typo

--- Evaluare_operator/main.py
+++ Evaluare_operator/main.py
@@ -5,7 +5,7 @@ Dupa 10 date gresite introduse de la tastatura, programul se opreste.
 """
 
 from module.operatii import listare, adaugare, stergere
-from module1git.validari import inregistreaza_eroare
+from module.validari import inregistreaza_eroare
 
 
 def meniu():
